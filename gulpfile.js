7364f411f3bca5b7b723dd656d34754ef6b736e2
Ant Stanley <antstanley@users.noreply.github.com>
2018-01-17 11:12:05 +0000

    more content and updates

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,28 @@ var uglify = require('gulp-uglify');
 var pump = require('pump');
 var browserSync = require('browser-sync').create();
 
-gulp.task('compress', function (cb) {
-  pump([
-        gulp.src('src/*.js'),
-        uglify(),
-        gulp.dest('dist')
-    ],
-    cb
-  );
+gulp.task('compress', function(cb) {
+    pump([
+            gulp.src('src/*.js'),
+            uglify(),
+            gulp.dest('dist')
+        ],
+        cb
+    );
 });
- 
+
 gulp.task('minify', function() {
-  return gulp.src('src/html/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'));
+    return gulp.src('src/html/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('copy', function(){
-  return gulp.src(['src/_redirects','src/*.xml'])
-    .pipe(gulp.dest('dist'));
+gulp.task('copy', function() {
+    return gulp.src(['src/_redirects', 'src/*.xml'])
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build',['minify','compress','copy']);
+gulp.task('build', ['minify', 'compress', 'copy']);
 
 
 // Static server
